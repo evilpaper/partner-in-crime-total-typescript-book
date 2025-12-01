b8de606724b31b30cf78db4cb9ffbb46d4a230e9
Pelle Lundgren <pellburt@gmail.com>
2025-12-01 07:20:00 +0100

    refactor(62): throw error to narrow type

--- src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
+++ src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
@@ -2,6 +2,10 @@ import { Equal, Expect } from "@total-typescript/helpers";
 
 const appElement = document.getElementById("app");
 
+if (!appElement) {
+  throw new Error("App element not found");
+}
+
 // How do I ensure that appElement is defined?
 
 type Test = Expect<Equal<typeof appElement, HTMLElement>>;
